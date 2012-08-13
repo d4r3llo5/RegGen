790344514bce83485a0b4d01b42708db8d4896cf
d4r3llo5 <d4r3llo5@gmail.com>
2012-08-13 16:49:07 -0500

    The first commit

---
+++ RegGen/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegGen
+{
+    class DSNGenerator
+    {
+
+        /*
+         * Constructor:
+         */
+        public DSNGenerator() 
+        {
+
+        }
+
+        /*
+         * createDSN: creates a DSN file for 
+         *  the user to input into the registry
+         */
+        private void createDSN()
+        {
+
+        } /* end of method createDSN */
+
+        static void Main(string[] args)
+        {
+
+                /* Set up a DSN based on user input */
+            DSNGenerator newDSN;
+
+            newDSN = new DSNGenerator();
+            newDSN.createDSN();
+
+            Console.ReadLine();
+        }
+    }
+}
